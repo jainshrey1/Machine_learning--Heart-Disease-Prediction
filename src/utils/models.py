d1a3c9e48d80a44846e2488c7960315e54f6af20
Alber <makaryanalber@gmail.com>
2024-04-17 13:41:47 -0700

    the structure of model training

---
+++ src/utils/models.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+
+
+def logistic_regression(X,y):
+    
+    """
+    Pipeline with GridSearch to find the best LogRegression model
+    """
+    
+    
+    
+    
+    
